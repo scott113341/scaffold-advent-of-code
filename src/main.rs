349782031c3b8f4db4e93a0d76e880fc921d5bbf
Scott Hardy <scott.the.hardy@gmail.com>
2018-12-02 15:44:05 -0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![feature(dbg_macro)]
+
+fn main() {
+    let input = include_str!("input.txt")
+        .trim()
+        .split("\n")
+        .map(|s| s.to_string())
+        .collect();
+
+    println!("part_1: {}", part_1(&input));
+    println!("part_2: {}", part_2(&input));
+}
+
+fn part_1(lines: &Vec<String>) -> usize {
+    1
+}
+
+fn part_2(lines: &Vec<String>) -> usize {
+    2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+    }
+
+    #[test]
+    fn test_part_2() {
+    }
+}
