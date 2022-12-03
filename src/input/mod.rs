20ed1bffede09483fcd94b971e83e68376e6fe15
Scott Hardy <scott.the.hardy@gmail.com>
2022-12-02 19:46:59 -0700

    Fix Clippy lint

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -8,7 +8,7 @@ where
 {
     include_str!("input.txt")
         .trim()
-        .split("\n")
+        .split('\n')
         .map(|s| s.parse().unwrap())
         .collect()
 }
@@ -21,7 +21,7 @@ where
 {
     include_str!("test_input.txt")
         .trim()
-        .split("\n")
+        .split('\n')
         .map(|s| s.parse().unwrap())
         .collect()
 }
